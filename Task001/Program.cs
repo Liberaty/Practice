432601186609678aeb6f73c5f59497620cd4f584
Liberaty <lepishin.ao@yandex.ru>
2022-08-10 23:43:25 +0500

    Add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+int Prompt(string message)
+{
+    System.Console.Write(message);                                                  // вывести сообщение
+    string readValue = Console.ReadLine();                                          // считывает строку с консоли
+    int result = int.Parse(readValue);                                              // преобразует строку в целое число
+    return result;                                                                  // возвращает результат
+}
+
+int value;                                                                          // объявляем переменную
+value = Prompt("Введите число: ");                                                  // вводим значение
+int result = value * value;                                                         // вычисляем квадрат
+System.Console.WriteLine($"Квадрат числа {value} равен {result}");                  // Вывод результата
